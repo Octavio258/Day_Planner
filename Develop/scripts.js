fc78fb471fb69d147f99dba18a088c8330801353
Octavio Politron <octavio.politron@gmail.com>
2022-01-27 04:05:39 -0800

    added click function and local storage stuff to scripts

---
+++ Develop/scripts.js
@@ -0,0 +1,26 @@
+const StartDate = $('#currentDay').text(moment().format('dddd, MMMM YYYY'));
+const currentTime = $('#currentTime').text(moment().format("LT"));
+
+
+const now = moment().hours();
+
+$(document).ready(function () {
+    $('.saveBtn').on('click', function() {
+        var usrText = $(this).siblings('.description').val();
+        var hour = $(this).parent().attr('id');
+        localStorage.setItem(hour, usrText);
+    });
+
+    $('#Iam9 .description').val(localStorage.getItem('Iam9'));
+    $('#Iam10 .description').val(localStorage.getItem('Iam10'));
+    $('#Iam11 .description').val(localStorage.getItem('Iam11'));
+    $('#Iam12 .description').val(localStorage.getItem('Iam12'));
+    $('#Iam1 .description').val(localStorage.getItem('Iam1'));
+    $('#Iam2 .description').val(localStorage.getItem('Iam2'));
+    $('#Iam3 .description').val(localStorage.getItem('Iam3'));
+    $('#Iam4 .description').val(localStorage.getItem('Iam4'));
+    $('#Iam5 .description').val(localStorage.getItem('Iam5'));
+
+
+
+    });
